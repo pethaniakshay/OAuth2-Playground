76e479203e1cbdd4c2423f4b524c9bb4be029049
Akshay Pethani <pethaniakshay@users.noreply.github.com>
2019-07-17 18:51:59 +0530

    Initial Commit

---
+++ inmemory-authorization-server/src/main/java/com/codepuran/imas/InMemoryAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package com.codepuran.imas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InMemoryAuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InMemoryAuthorizationServerApplication.class, args);
+	}
+
+}
